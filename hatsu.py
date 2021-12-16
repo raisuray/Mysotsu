58648228fb5394d35490e75f56575ec980ba449f
ray <raymond.widjaja10@gmail.com>
2021-12-16 16:02:48 +0900

    fix gitigonre add some to hatsumei.py

--- hatsu.py
+++ hatsu.py
@@ -1,4 +1,18 @@
-import spacy 
+import os
+import spacy as spacy
+import json
+from mymodule.Patent import Patent
 
-from mymodule.myJson import *
+path = './effect_words/'
+effect_words = os.listdir(path)
+dict_eff_words = dict.fromkeys(effect_words)
+
+patents_list = []
+
+for patent in dict_eff_words.keys():
+    file_path = path+patent
+    new = Patent(file_path)
+    patents_list.append(new)
+
+print(patents_list)
 
