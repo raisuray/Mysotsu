d158803200d1eb27735eec12c15c2e8718faabcd
ray <raymond.widjaja10@gmail.com>
2021-12-16 16:40:10 +0900

    update hatsumei.py

--- hatsu.py
+++ hatsu.py
@@ -2,6 +2,8 @@ import os
 import spacy as spacy
 import json
 from mymodule.Patent import Patent
+import pprint
+import regex
 
 path = './effect_words/'
 effect_words = os.listdir(path)
@@ -14,5 +16,23 @@ for patent in dict_eff_words.keys():
     new = Patent(file_path)
     patents_list.append(new)
 
-print(patents_list)
+
+pattern = "[(.*ように)]"
+nlp = spacy.load("ja_ginza")
+
+for patent in patents_list:
+    print(patent.name)
+    inv_words = patent.doc_invention
+
+    for inv_word in inv_words:
+        sentences = inv_word.split("、")
+
+        for sentence in sentences:
+            doc = nlp(sentence)
+            for span in doc:
+                print(span.text, span.head, span.)
+
+
+        break
+    break
 
